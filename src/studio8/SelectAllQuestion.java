484b98368198a647dd3b5502c00a208080cec109
Doug Shook <dshook@wustl.edu>
2023-11-11 08:26:13 -0600

    reconfiguring studio

---
+++ src/studio8/SelectAllQuestion.java
@@ -0,0 +1,31 @@
+package studio8;
+
+public class SelectAllQuestion extends MultipleChoiceQuestion {
+
+	public SelectAllQuestion(String prompt, String answer, int points, String[] choices) {
+		//FIXME
+	}
+	
+	public int checkAnswer(String givenAnswer) {
+		//FIXME Should return partial credit (if earned)!
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		String[] choices = {"instance variables", "git", "methods", "eclipse"};
+		Question selectAll = new SelectAllQuestion("Select all of the following that can be found within a class:", "13", 4, choices);
+		selectAll.displayPrompt();
+		System.out.println(selectAll.checkAnswer("hi")); //no credit
+		System.out.println(selectAll.checkAnswer("2")); //no credit
+		System.out.println(selectAll.checkAnswer("13")); //full credit
+		System.out.println(selectAll.checkAnswer("31")); //full credit
+		System.out.println(selectAll.checkAnswer("1")); //3 points
+		System.out.println(selectAll.checkAnswer("3")); //3 points
+		System.out.println(selectAll.checkAnswer("23")); //2 points
+		System.out.println(selectAll.checkAnswer("34")); //2 points
+		System.out.println(selectAll.checkAnswer("4")); //1 point
+		System.out.println(selectAll.checkAnswer("124")); //1 point
+		System.out.println(selectAll.checkAnswer("24")); //0 points
+		
+	}
+}
